5e02caeb8f40ef9427e3ecfebf5f519e5c753bda
Bernard Mak <b2mak2@gmail.com>
2023-05-22 20:48:17 -0400

    Bunch of minor updates

--- site/src/main.rs
+++ site/src/main.rs
@@ -1,15 +1,33 @@
-#[actix_web::get("/")]
-async fn hello() -> impl actix_web::Responder {
+const PAGE_SIZE: i64 = 20;
+
+#[derive(serde::Deserialize)]
+struct OrdersQuery {
+  page: Option<usize>,
+}
+
+#[actix_web::get("/orders")]
+async fn orders(
+  info: actix_web::web::Query<OrdersQuery>,
+) -> impl actix_web::Responder {
   let mut conn = common::database::establish_connection();
-  let orders = common::database::get_orders(&mut conn);
+  let page = if info.page.is_some() { info.page.unwrap() } else { 0 };
+  let orders = common::database::get_orders(
+    &mut conn,
+    PAGE_SIZE,
+    i64::try_from(page).unwrap(),
+  );
   let orders_json =
     serde_json::to_string(&orders).expect("couldn't serialize orders");
-  actix_web::HttpResponse::Ok().body(orders_json)
+  actix_web::HttpResponse::Ok()
+    .content_type(actix_web::http::header::ContentType::json())
+    .body(orders_json)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-  actix_web::HttpServer::new(|| actix_web::App::new().service(hello))
+  actix_web::HttpServer::new(|| actix_web::App::new().service(orders))
+    // Setting to 2 workers for now, should change during production
+    .workers(2)
     .bind(("0.0.0.0", 8080))?
     .run()
     .await
