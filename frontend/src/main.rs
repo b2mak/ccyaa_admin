1456a174d1088ffbbb46781f21fd03d2baccefb9
Bernard Mak <b2mak2@gmail.com>
2023-07-02 13:04:08 -0400

    Get a basic yew app working

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,12 @@
+use yew::prelude::*;
+
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello World" }</h1>
+    }
+}
+
 fn main() {
-  println!("Default main");
+    yew::Renderer::<App>::new().render();
 }
